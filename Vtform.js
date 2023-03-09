965a92b6eb5c0bcddebc109a85628ab6d6e08b0f
Rishabh Rathore <Rish.rathore9@gmail.com>
2023-03-09 16:20:40 +0530

    vtForm added

---
+++ Vtform.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Vtform = () => {
+    return (
+        <div>
+            <label>Phone</label>
+            <input type="text" value="bcd" />
+            <label>Email</label>
+            <input type="text" value="bcd" />
+            <label>Card</label>
+            <input type="text" value="abc" />
+        </div>
+    )
+}
+
+export default Vtform
